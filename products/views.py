90be2cf2790269a7aabdc881ded9a712f44c9912
RNT13 <renatornt13@gmail.com>
2025-11-05 10:13:41 -0300

    Update: - Remoção da autenticação da view de produtos

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from rest_framework.authentication import (
-    BasicAuthentication,
-    SessionAuthentication,
-    TokenAuthentication,
-)
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import (
+#     BasicAuthentication,
+#     SessionAuthentication,
+#     TokenAuthentication,
+# )
+# from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Product
@@ -11,12 +11,12 @@ from .serializers import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [
-        TokenAuthentication,
-        SessionAuthentication,
-        BasicAuthentication,
-    ]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [
+    #     TokenAuthentication,
+    #     SessionAuthentication,
+    #     BasicAuthentication,
+    # ]
+    # permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     queryset = Product.objects.all().order_by("id")
