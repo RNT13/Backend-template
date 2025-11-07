2011d60cc8e6a853ddd7fe17a9ec8882102993e4
RNT13 <renatornt13@gmail.com>
2025-11-07 10:54:33 -0300

    Fix

--- core/settings.py
+++ core/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "debug_toolbar",
     "django_extensions",
     "products",
     "orders",
